57af2b85b95d6ee787bbb7c78c4a42875e9ff3e9
Matt Deeds <mdeeds@gmail.com>
2015-09-23 20:28:45 -0700

    Add 'CopyFrom' to vector.js

--- vector.js
+++ vector.js
@@ -42,6 +42,11 @@ Vector2D.prototype.Add = function(that) {
 	this.y = this.y + that.y;
 }
 
+Vector2D.prototype.CopyFrom = function(other) {
+	this.x = other.x;
+	this.y = other.y;
+}
+
 Vector2D.prototype.toString = function() {
 	return "(" + this.x + ", " + this.y + ")";
 }
