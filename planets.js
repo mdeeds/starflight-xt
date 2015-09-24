51d178ae106e9f323a7b61fd8d49d363c89aef69
Matt Deeds <mdeeds@gmail.com>
2015-09-23 20:28:11 -0700

    Update some numbers for mass etc. and add more references

--- planets.js
+++ planets.js
@@ -14,7 +14,8 @@ var OmegaFromPeriod = function(days) {
 
 // https://en.wikipedia.org/wiki/Earth%27s_orbit
 // Period: 365.256 days
-// Radius: 149.59787e9 meters
+// Orbit Radius: 149.59787e9 meters
+// Body Radius: 6.371e6
 var EarthOrbit = new RotatingFrame(SunFrame, 
     new Attitude(new Vector2D(), OmegaFromPeriod(365.256)));
 
@@ -24,10 +25,20 @@ var EarthMoonFrame = new RotatingFrame(EarthOrbit,
     new Attitude(new Vector2D(149.59787e9, 0), OmegaFromPeriod(27.322)));
 	
 // Earth and moon orbit each other.  
+// Earth orbit radius: 1.49598261000e11 m  https://en.wikipedia.org/wiki/Earth
+// Earth body radius: 6.371000e6 m         https://en.wikipedia.org/wiki/Earth
+// Earth mass: 5.97237e24 kg
+//
 // https://en.wikipedia.org/wiki/Barycenter
 //  a = 384e6
-//  r1 = 4.670e6
-//  r2 = 379e6
+//  r1 = 4.676e6
+//  r2 = 3.801e8
+// ** MOON **
+// Moon orbit radius: 3.84748000e8 m  https://en.wikipedia.org/wiki/Orbit_of_the_Moon
+// Moon body radius: 1.737100e6 m  https://en.wikipedia.org/wiki/Moon
+// Mass 7.3477e22 kg
+// Geosynchronous radius: 4.2164e7 https://en.wikipedia.org/wiki/Geosynchronous_orbit
+// Earth Moon L1 radius: 
 var EarthFrame = new RotatingFrame(EarthMoonFrame, 
     new Attitude(new Vector2D(4.670e6, 0), OmegaFromPeriod(1)));
 var Earth = new SolidObject(new Vector2D(), 5.972e24, 6.371e6);
